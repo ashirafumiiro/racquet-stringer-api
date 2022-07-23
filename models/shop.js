405bb4c525c0fc50dbdb71b52ad08ad0f21e4025
ashirafumiiro <ashirafumiiro@gmail.com>
2022-07-23 10:30:36 +0300

    modification to shop

--- models/shop.js
+++ models/shop.js
@@ -31,8 +31,11 @@ var ShopSchema = new Schema({
   allow_own_strings: {type: Boolean},
   stripe_customer_id: {type: String},
   stripe_subscription_id: {type: String},
-  stripe_account_id: {type: String},
-  tax: {type: Number},
+  stripe_account_id: {type: String, default: ''},
+  tax: {type: Number, default: 0},
+  stripe_price_id: {type: String, default: ''},
+  subscripiton_expiry: {type: Date},
+  comission: {type: Number},
   stripe_status: {type: String, enum: ["enabled", "disabled", "suspended"], default: "disabled"}
 }, opts);
 
